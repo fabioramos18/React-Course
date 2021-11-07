8b8856fc1692de6ed8a2c4a9bb9ca11dd421637b
Fabio Ramos <fabioramos10h@gmail.com>
2021-11-07 19:27:41 +0000

    Const and Variables

--- initial-course-of-react/src/App.js
+++ initial-course-of-react/src/App.js
@@ -1,10 +1,18 @@
 import React from 'react'
 
 const App = () => {
+  const title=()=>{
+    return('Const and Variables')
+  }
+
+  const channel = 'CFB'
+  const course = 'React'
+
   return (
     <div>
-      <h1>React Course</h1>
-      <p>Hello word !</p>
+      <h1>{title()}</h1>
+      <p>Title: {channel}</p>
+      <p>{'Course: '+ course}</p>
     </div>
   )
 }

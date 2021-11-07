477eeaec5e73291fb687e654f8d978a5bc5697a3
Fabio Ramos <fabioramos10h@gmail.com>
2021-11-07 19:57:53 +0000

    create components

--- initial-course-of-react/src/App.js
+++ initial-course-of-react/src/App.js
@@ -1,25 +1,13 @@
 import React from 'react'
-import Logo from './components/img/Capturar2.JPG'
+import { Header } from './components/Header'
+import { Body } from './components/Body'
 
 const App = () => {
-  const title=()=>{
-    return('Const and Variables')
-  }
-
-  const channel = 'CFB'
-  const course = 'React'
-
   return (
-    <div>
-      <header>
-      <h1>{title()}</h1>
-      </header>
-      <p>Title: {channel}</p>
-      <p>{'Course: '+ course}</p>
-      <img alt='' src={Logo}/>
-      <br/>
-      <img alt='' src='/Capturar3.JPG'></img>
-    </div>
+    <>
+      <Header/>
+      <Body/>
+    </>
   )
 }
 
---
+++ initial-course-of-react/src/components/Body.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const Body = () => {
+    return (
+        <div>
+            <h2>React Course</h2>
+            <p>Training React components</p>
+        </div>
+    )
+}

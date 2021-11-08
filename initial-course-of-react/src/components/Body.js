65dea2b7f8e3e4033e3b99b1cb5c014288f888f1
Fabio Ramos <fabioramos10h@gmail.com>
2021-11-08 03:11:29 +0000

    useState

--- initial-course-of-react/src/components/Body.js
+++ initial-course-of-react/src/components/Body.js
@@ -1,11 +1,17 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './Body.css'
 
 export const Body = () => {
+
+    const [num,setNum]=useState(10)
+
     return (
         <div>
             <h2>React Course</h2>
-            <p className='text'>Training React CSS</p>
+            <p className='text'>Training React useState</p>
+            <h1>State Number: {num}</h1>
+            <button onClick={()=>setNum(num+10)}> + 10 </button>
+
         </div>
     )
 }

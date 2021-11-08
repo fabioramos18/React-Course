b855461c2bf11c71b5ca530394bec6f91e663bdc
Fabio Ramos <fabioramos10h@gmail.com>
2021-11-08 11:12:57 +0000

    conditions

--- initial-course-of-react/src/App.js
+++ initial-course-of-react/src/App.js
@@ -1,26 +1,63 @@
 import React, {useState} from 'react'
-import { Header } from './components/Header'
-import { Body } from './components/Body'
-import { Data } from './components/Data'
-import LedVerde from './components/img/verde.jpg'
-import LedVermelho from './components/img/vermelho.JPG'
-import './App.css'
+
 const App = () => {
-  const name = () =>{
-    return('Ramos')
+
+  const[log,setLog]=useState(false)
+  const[color,setColor]=useState(1)
+
+  const msgLogin=()=>{
+    return 'Logout'
+  }
+  const msgLogOff=()=>{
+    return 'Please Login'
+  }
+  const greet=()=>{
+    const hour= new Date().getHours()
+    if(hour >= 0 && hour<13){
+      return <p>Good morning</p>
+    }
+    else if(hour>13 && hour<18){
+      return <p>Good afternoon</p>
+    }
+    else{
+      return <p>Good night</p>
+    }
   }
 
-  const youtube = 'youtube.com'
+  const red ={color:'#f00'}
+  const green ={color:'#0f0'}
+  const blue ={color:'#00f'}
 
-  const [btOn,setButtonOn]=useState(false)
+  const returnColor=(c)=>{
+    if(c===1){
+      return red
+    }
+    else if(c===2){
+      return green
+    }
+    else{
+      return blue
+    }
+  }
+
+  const changeColor=()=>{
+    setColor(color+1)
+    if(color >2){
+      setColor(1)
+    }
+  }
+
   return (
     <div className='center'>
-      <Header/>
-      <Body/>
-      <Data youtube={youtube} name={name()} day='2'/>
+      <h1 style={returnColor(color)}>React Course</h1>
+      {greet()}
+      <button onClick={()=>changeColor()}>Change Color</button>
 
-      <img alt='' style={{width:'50px'}} src={btOn?LedVerde:LedVermelho}></img>
-      <button onClick={()=>setButtonOn(!btOn)}>{btOn?'OFF':'ON'}</button>
+      <p>{log?msgLogOff():msgLogin()}</p>
+      <button onClick={()=>setLog(!log)}>{log?msgLogOff():msgLogin()}</button>
+      <br/>
+
+      
     </div>
   )
 }

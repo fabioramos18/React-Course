6f42ff7cb28b7f8ca64529d06b33e9bbd26d59b8
Fabio Ramos <fabioramos10h@gmail.com>
2021-11-08 10:30:46 +0000

    Events

--- initial-course-of-react/src/App.js
+++ initial-course-of-react/src/App.js
@@ -1,7 +1,9 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { Header } from './components/Header'
 import { Body } from './components/Body'
 import { Data } from './components/Data'
+import LedVerde from './components/img/verde.jpg'
+import LedVermelho from './components/img/vermelho.JPG'
 import './App.css'
 const App = () => {
   const name = () =>{
@@ -9,11 +11,16 @@ const App = () => {
   }
 
   const youtube = 'youtube.com'
+
+  const [btOn,setButtonOn]=useState(false)
   return (
     <div className='center'>
       <Header/>
       <Body/>
       <Data youtube={youtube} name={name()} day='2'/>
+
+      <img alt='' style={{width:'50px'}} src={btOn?LedVerde:LedVermelho}></img>
+      <button onClick={()=>setButtonOn(!btOn)}>{btOn?'OFF':'ON'}</button>
     </div>
   )
 }
